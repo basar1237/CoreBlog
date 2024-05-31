ebc52ab595556f587816778ec15115434fd2d0b8
basar1237 <basaryldrm1237@gmail.com>
2024-05-31 10:46:06 +0300

    Add project files.

---
+++ Controllers/CategoryCotroller.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace demo1.Controllers
+{
+    public class CategoryCotroller : Controller
+    {
+        CategoryManager cm=new CategoryManager(new EFCategoryRepostory());   
+        public IActionResult Index()
+        {
+            var values = cm.GetList();
+            return View( values);
+        }
+    }
+}
